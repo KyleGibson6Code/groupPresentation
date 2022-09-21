cd52f8bd2cfebd49677e994bcb0b5b6d025bd1d9
KyleGibson6Code <kylegibson3gs@gmail.com>
2022-09-20 20:35:05 -0400

    Group Work

---
+++ groupPresentation.java
@@ -0,0 +1,22 @@
+package groupAssignment;
+
+public class groupPresentation {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+String Str = new String("Hello World");
+		
+		System.out.println(Str.toUpperCase() );
+		
+		
+		
+		System.out.println(Str.toLowerCase() ); 
+
+		
+			
+		}
+	
+	}
+
+
